9c391a6a3a050fd50c0cea151d6563e87cf45765
Marina Belova <83077065+Salirsky@users.noreply.github.com>
2022-06-10 15:10:32 +0300

    Add methods to classes Person & FantasyHero

--- script.js
+++ script.js
@@ -1,20 +1,5 @@
 "use strict";
 
-// Использовать наследование (главный класс - Person, наследуемый - RacePerson)
-// Каждый класс должен содержать конструктор, геттеры и сеттеры, а также методы, которые нужны в нём по смыслу.
-//Базовый класс должен состоять из минимум 4 свойств различных типов.
-// Каждый класс - наследник должен расширять базовый класс и включать два или более дополнительных свойства.
-
-// Сверстать страницу. На странице должна быть форма и таблица.
-
-//  При Сохранении данных из формы создаем объект на основе выбранного класса-наследника. Записываем каждую сущность в единый массив.
-
-// Данные из объектов в массиве должны сохраняться в localstorage и выводиться на страницу в таблице
-
-// При обновлении страницы данные должны сохраняться
-
-//При нажатии кнопки "Удалить" данный объект должен быть удален из массива данных, со страницы и с localstorage. Для удаления использовать метод класса!
-
 const buttonCreate = document.querySelector(".button-create");
 
 const table = document.querySelector(".table-saved-person");
@@ -25,62 +10,34 @@ const tableTr = document.querySelector("tr");
 const heroName = document.querySelector(".form-name");
 const heroAge = document.querySelector(".form-age");
 const heroGenders = document.querySelectorAll(".gender-checkbox");
-
-// const heroRace = document.getElementsByName("race-select");
-const heroRace = document.querySelectorAll("race-select");
-const heroLook = document.getElementsByClassName(".form-look");
-
-const heroCharacter = document.getElementsByClassName(".form-character");
-const heroHistory = document.getElementsByClassName(".form-history");
-const heroSkill = document.getElementsByClassName(".form-skill");
-const heroPockets = document.getElementsByClassName(".form-pockets");
-
-let heroGender;
-
-// console.log(heroName.value);
-// console.log(heroAge.value);
-// console.log(heroGenders);
-console.log(heroRace);
-
-//Метод для добавления информации про пол персонажа:
-const addGender = () => {
-  for (const gender of heroGenders) {
-    if (gender.checked) {
-      heroGender = gender.value;
-    }
-  }
-};
-
-//Метод для добавления информации про расу персонажа:
-const addRace = () => {
-  heroRace.forEach(function (race, index) {
-    const select = race.querySelector("select");
-    const input = race.querySelector("input");
-    const selectName = select.options[select.selectedIndex].textContent;
-
-    console.log(select);
-    console.log(input);
-    console.log(selectName);
-  });
-};
+const heroRaces = document.querySelectorAll(".race-select");
 
 let heroes = []; // массив объектов, где каждый объект - отдельный персонаж
 
-// Пример создаваемого персонажа, используется только сначала.
+let heroGender; // Выбранный пол
+let heroRace; // Выбранная раса
 
-// const a = "Терентий";
-// const b = "90";
-// const c = "Мужчина";
-const d = "Характер паскудный";
-const e =
-  "Все думали, там умер дед, но дед не умирал! Ему 90, он не помнит стоп-слово :D";
-const f = "Скромное ничего";
-const g = "Гипноз";
-const h = "Пушистый хвост";
+//Метод для добавления информации про пол персонажа:
+// const addGender = () => {
+// for (const gender of heroGenders) {
+//   if (gender.checked) {
+//     heroGender = gender.value;
+//   }
+// }
+// };
+
+// console.log(heroRaces);
+
+// Метод для добавления информации про расу персонажа:
+// const addRace = () => {
+// heroRaces.forEach(function (race) {
+//   heroRace = race.options[race.selectedIndex].text;
+// });
 
 // При нажатии на кнопку "Создать!" :
 
 const start = () => {
+  // addRace();
   createHero();
   // createTr();
   // pushHeroToTable();
@@ -97,13 +54,10 @@ const createHero = () => {
   const newHero = new FantasyHero(
     heroName.value,
     heroAge.value,
-    heroGender,
-    d,
-    e,
-    f,
-    g,
-    h
+    heroGenders,
+    heroRaces
   );
+
   // Отправляем нового героя в массив героев
   heroes.push(newHero);
   // Клонируем строку в таблице
@@ -115,88 +69,42 @@ const createHero = () => {
   nodeTd[1].innerText = newHero.age;
   nodeTd[2].innerText = newHero.gender;
   nodeTd[3].innerText = newHero.race;
-  nodeTd[4].innerText = newHero.look;
-  nodeTd[5].innerText = newHero.character;
-  nodeTd[6].innerText = newHero.history;
-  nodeTd[7].innerText = newHero.skill;
-  nodeTd[8].innerText = newHero.pockets;
-  nodeTd[9].innerHTML = '<button class="button button-delete">Удалить</button>';
+  nodeTd[4].innerHTML = '<button class="button button-delete">Удалить</button>';
 
-  console.log(heroName.value);
-  console.log(heroAge.value);
-  console.log(heroGender.checked);
+  console.log(heroes);
 };
-
-// const createTr = () => {
-// };
 
 // Создаём исходный класс
 class Person {
-  constructor(name, age, gender, character, history, pockets) {
+  constructor(name, age, heroGenders) {
     this.name = name;
     this.age = age;
-    this.gender = gender;
-    this.character = character;
-    this.history = history;
-    this.pockets = pockets;
-    // имя
-    // возраст
-    // пол
-    // характер
-    // история
-    // карманцы
+    this.heroGenders = heroGenders;
+  }
+  //Метод для добавления информации про пол персонажа:
+  addGender(heroGenders) {
+    for (const gender of heroGenders) {
+      if (gender.checked) {
+        this.heroGender = gender.value;
+        return this.heroGender;
+      }
+    }
+    console.log(`Привет! я ${this.heroGender}!`);
   }
 }
 
 // Расширяем исходный класс до фентезийного персонажа
 class FantasyHero extends Person {
-  constructor(name, age, gender, character, history, pockets, skill, look) {
-    super(name, age, gender, character, history, pockets);
-    this.skill = skill;
-    this.look = look;
-    this.race = "Человек";
-    // Способность
-    // Внешность
-    // Раса
+  constructor(name, age, gender, race) {
+    super(name, age, gender);
+    this.race = race;
   }
-
-  // heroes.push({}); // это не сработает, но идея - пушить в массив методом этого класса
-
-  // При создании каждого нового экземпляра класса FantasyHero этот экземпляр должен:
-  // записываться в массив объектов;
-  // записываться в таблицу, создавая в ней новую колонку (или строку)
-  // сохраняться в localstorage
-
-  // Также здесь должен быть метод для удаления экземпляра
-  // - Из таблицы
-  // - Из localstorage
+  //Метод для добавления информации про расу персонажа:
+  addRace(heroRaces) {
+    heroRaces.forEach(function (race) {
+      heroRace = race.options[race.selectedIndex].text;
+    });
+  }
 }
 
 buttonCreate.addEventListener("click", start);
-
-// console.log(Person);
-// console.log(FantasyHero);
-// console.log(hero1);
-
-//---------------------------------------------------------
-
-// Это кусок кода, наглядно демонструрующий работу наследования классов, и он пока побудет здесь.
-
-// class First {
-//   hello() {
-//     console.log("Привет, я метод родителя!");
-//   }
-// }
-
-// class Second extends First {
-//   hello() {
-//     super.hello();
-//     console.log("А я наследуемый метод!");
-//   }
-// }
-
-// const one = new First();
-// one.hello();
-
-// const two = new Second();
-// two.hello();
